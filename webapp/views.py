ae1bac31b088f0d75327845ddda43546b5025088
machageDev <machagefranklyn@gmail.com>
2025-10-05 11:49:56 +0300

    added functions http request

--- webapp/views.py
+++ webapp/views.py
@@ -3,3 +3,16 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request,'index.html')
+
+def projects(request):
+    return render(request, 'webapp/projects.html')
+
+def contact(request):
+    if request.method == "POST":
+        
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        message = request.POST.get('message')
+        
+        print(f"Message from {name} ({email}): {message}")
+    return render(request, 'contact.html')
